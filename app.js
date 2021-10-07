c2dc4405b45da078df0a5e53d47ac35ba3121736
udaibirv <udaibirv@gmail.com>
2021-10-07 14:57:49 -0700

    learned and used type alias and union types in typescript.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-//union type
-function combine(input1, input2) {
+//union type = |
+//literal types =
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine('Max', 'Anna');
+var combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
