4c3abab68c155710bdb640ecda53de686501d2e3
udaibirv <udaibirv@gmail.com>
2021-10-12 13:52:11 -0700

    learned about spread operator, rest parameter, and reduce method in typescript.

--- cl-interfaces-01-starting-setup/dist/app.js
+++ cl-interfaces-01-starting-setup/dist/app.js
@@ -1,2 +1,18 @@
 "use strict";
+const userName = 'Max';
+const hobbies = ['sports', 'cooking'];
+const activeHobbies = ['Hiking'];
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+const person = {
+    name: 'max',
+    age: 20
+};
+const copiedPerson = Object.assign({}, person);
+const add = (...numbers) => {
+    return numbers.reduce((currentResult, currentValue) => {
+        return currentResult + currentValue;
+    }, 0);
+};
+const addNumbers = add(5, 10, 15, 20);
 //# sourceMappingURL=app.js.map
--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+const userName = 'Max';
+// const add = (a: number, b: number) => a + b;
+// console.log(add(5, 12));
+
+
+const hobbies = ['sports', 'cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies); //spread operator = pulling out elements in an array or objects
+
+console.log(activeHobbies);
+
+const person = {
+  name: 'max',
+  age: 20
+};
+
+const copiedPerson = {...person}; //spread operator in objects
+
+const add = (...numbers: number[]) => { // rest paramter to allow for n number of paramters
+  return numbers.reduce((currentResult, currentValue) => {
+    return currentResult + currentValue;
+  }, 0)  //perfroms an operation on every element in array and adds currentResult and currentValue and returns, second argument is currentValue which is 0
+}
+
+const addNumbers = add(5, 10, 15, 20);
