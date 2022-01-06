09edc28c195f06bba0dca01f3bdb1e24ac4d2ff8
udaibirv <udaibirv@gmail.com>
2022-01-05 16:07:47 -0800

    created classes and child classes through inheritance in typescript.

--- typescript-section-5/src/app.ts
+++ typescript-section-5/src/app.ts
@@ -1,15 +1,16 @@
 // Code goes here!
 class Department{
-  private name: string;
+
   private employees: string[] = [] //an array with string values for Employees
   //private keyword now makes employees array only accessible within Department class (all methods too)
-  constructor(n: string){  //function tied to this class, which is executed when object is being created
-    this.name = n;
+  constructor(private readonly id: string, public name: string){  //function tied to this class, which is executed when object is being created
+    // this.id = id;
+    // this.name = n;
   }
 
   describe(this:Department) { //method defined within Department Class. This is passed as parameter which has a type of the class
                               //Will always refer to an isntance of an object which alwasys referring to Department
-    console.log('Department: ' + this.name);
+    console.log(`Department${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string){ //Gets the value passed with this method, and pushes it to employees array defined above
@@ -22,14 +23,42 @@ class Department{
   }
 }
 
-const accounting = new Department('Accounting');
+class ITDepartment extends Department{ //inehrits from Department class, gets everything including it's constructor.
+  admins: string[];
+  constructor(id: string, admins: string[]){
+    super(id, 'IT'); //whenever you add new constructor from class that inheirts from a parent, need super();
+    this.admins = admins;
+  }
+}
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
+class AccountingDepartment extends Department{
+  constructor(id: string, private reports: string[]){
+    super(id, 'ACCT')
+  }
+  addReport(text: string){
+    this.reports.push(text);
+  }
+
+  getReports(){
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('d1', ['Max']);
+
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.addReport('Something went wrong. CEO is dodging taxes');
+
+accounting.getReports();
+
+it.addEmployee('Max');
+it.addEmployee('Manu');
 //Adds new employees to the employee array
 
-accounting.describe();
-accounting.printEmployeeInformation();
+it.describe();
+it.printEmployeeInformation();
+console.log(it);
 
 // const accountingCopy = {name: 'Finanace', describe: accounting.describe}
 // accountingCopy.describe();
